a0a6d8b74f92a10a844b53b895e18aff239bac52
Junior Ngangeli <juniorngangeli3@gmail.com>
2022-09-24 09:07:33 +0100

    feat: implement multi lane road

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ canvas.width = 200;
 
 const context = canvas.getContext("2d");
 
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(3), 100, 30, 50);
 car.draw(context);
 
 animate();
@@ -11,6 +12,7 @@ animate();
 function animate() {
   canvas.height = window.innerHeight;
   car.update();
+  road.draw(context);
   car.draw(context);
   requestAnimationFrame(animate);
 }
